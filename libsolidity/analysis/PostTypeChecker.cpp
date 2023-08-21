25083930b474095dd748dbc401974998d5ec01fd
Matheus Aguiar <matheus.pit@gmail.com>
2023-08-21 15:45:13 -0300

    fixup! Special case code generation for for loops.

--- libsolidity/analysis/PostTypeChecker.cpp
+++ libsolidity/analysis/PostTypeChecker.cpp
@@ -439,8 +439,9 @@ public:
 	bool willBeWrittenTo() const { return m_willBeWrittenTo; }
 	void endVisit(Identifier const& _identifier) override
 	{
+		solAssert(_identifier.annotation().referencedDeclaration, "");
 		if (
-			_identifier.annotation().referencedDeclaration == m_declaration &&
+			*_identifier.annotation().referencedDeclaration == *m_declaration &&
 			_identifier.annotation().willBeWrittenTo
 		)
 			m_willBeWrittenTo = true;
