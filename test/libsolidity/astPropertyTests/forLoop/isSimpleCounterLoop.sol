12563a8e5a6b1506a22e181a5a1bc30bc1f4780c
Kamil Śliwak <kamil.sliwak@codepoets.it>
2023-08-25 12:52:58 +0200

    fixup! Add test

--- test/libsolidity/astPropertyTests/forLoop/isSimpleCounterLoop.sol
+++ test/libsolidity/astPropertyTests/forLoop/isSimpleCounterLoop.sol
@@ -1,24 +1,24 @@
 contract C {
-  function f() public pure {
-     /// PositiveCase1: isSimpleCounterLoop
-     for(uint i = 0; i < 42; ++i) {
-     }
-     /// PositiveCase2: isSimpleCounterLoop
-     for(int i = 0; i < 42; i++) {
-     }
-     /// NegativeCase1: isSimpleCounterLoop
-     for(uint i = 0; i < 42; i = i + 1) {
-     }
-     /// NegativeCase2: isSimpleCounterLoop
-     for(uint i = 42; i > 0; --i) {
-     }
-     /// NegativeCase3: isSimpleCounterLoop
-     for(uint i = 42; i > 0; i--) {
-     }
-     /// NegativeCase4: isSimpleCounterLoop
-     for(uint i = 1; i < 42; i = i * 2) {
-     }
-  }
+    function f() public pure {
+         /// PositiveCase1: isSimpleCounterLoop
+         for (uint i = 0; i < 42; ++i) {
+         }
+         /// PositiveCase2: isSimpleCounterLoop
+         for (int i = 0; i < 42; i++) {
+         }
+         /// NegativeCase1: isSimpleCounterLoop
+         for (uint i = 0; i < 42; i = i + 1) {
+         }
+         /// NegativeCase2: isSimpleCounterLoop
+         for (uint i = 42; i > 0; --i) {
+         }
+         /// NegativeCase3: isSimpleCounterLoop
+         for (uint i = 42; i > 0; i--) {
+         }
+         /// NegativeCase4: isSimpleCounterLoop
+         for (uint i = 1; i < 42; i = i * 2) {
+         }
+    }
 }
 // ----
 // PositiveCase1: true
